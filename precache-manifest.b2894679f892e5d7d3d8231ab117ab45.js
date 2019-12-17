5fa1552054b121cfcc45f990f88ea104d0dd0a7b
123aadina <adina.hamida@gmail.com>
2019-12-17 15:43:10 +0100

    Updates

---
+++ precache-manifest.b2894679f892e5d7d3d8231ab117ab45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d6b437ef8713a6066234943c9e281e6",
+    "url": "/React-Fetch/index.html"
+  },
+  {
+    "revision": "10528de263a063de7434",
+    "url": "/React-Fetch/static/css/main.7cc3caa5.chunk.css"
+  },
+  {
+    "revision": "b2cd8fa03f7d5b80ad1c",
+    "url": "/React-Fetch/static/js/2.843fec27.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/React-Fetch/static/js/2.843fec27.chunk.js.LICENSE"
+  },
+  {
+    "revision": "10528de263a063de7434",
+    "url": "/React-Fetch/static/js/main.d19d998e.chunk.js"
+  },
+  {
+    "revision": "58cf85d6ba0533eadbd8",
+    "url": "/React-Fetch/static/js/runtime-main.4e4b8543.js"
+  }
+]);
